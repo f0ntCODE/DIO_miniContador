bdbe7917f5c0731d0a733c0cf2716f302b613f8a
f0ntCODE <font3002@outlook.com>
2025-08-21 18:43:18 -0300

    Projeto finalizado; Possibilidade de escolha de inserção adicionada

--- contador/src/Main.java
+++ contador/src/Main.java
@@ -4,27 +4,47 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Main {
     public static void main(String[] args) {
        Scanner terminal = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro");
-        int parametroUm = terminal.nextInt();
+       int parametroUm = 0;
+       int parametroDois = 0;
+       boolean valorValido = false;
+       int opcao = 0;
 
-        System.out.println("Digite o segundo parâmetro");
-        int parametroDois = terminal.nextInt();
+       while (!valorValido){
+           System.out.println("Como deseja executar esta aplicação? " +
+                   "\n 1 = Inserção manual " +
+                   "\n 2 = Inserção automática");
 
-            //Implementar a possibilidade de escolher aleatório ou manual
+             opcao = terminal.nextInt();
+             valorValido = isValid(opcao);
+             if(!valorValido) System.out.println("\t As opções só podem ser 1 ou 2");
 
-       /* int parametroUm = ThreadLocalRandom.current().nextInt(100);
+       }
+
+        if(opcao == 1) {
+            //inseção manual
+
+            System.out.println("Digite o primeiro parâmetro");
+             parametroUm = terminal.nextInt();
+
+            System.out.println("Digite o segundo parâmetro");
+             parametroDois = terminal.nextInt();
+        }
+        else if(opcao == 2){
+            //inseção automática
+
+         parametroUm = ThreadLocalRandom.current().nextInt(100);
         System.out.println("Parâmetro 1 é: " + parametroUm);
-        int parametroDois = ThreadLocalRandom.current().nextInt(100);
+
+         parametroDois = ThreadLocalRandom.current().nextInt(100);
         System.out.println("Parâmetro 2 é: " + parametroDois);
 
-        */
-
-        try {
-            contar(parametroUm, parametroDois);
-        }catch (ParametrosInvalidosException ex) {
-
-            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
         }
+            try {
+                contar(parametroUm, parametroDois);
+            } catch (ParametrosInvalidosException ex) {
+
+                System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+            }
     }
     static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
         if(parametroUm > parametroDois){
@@ -36,6 +56,15 @@ public class Main {
         System.out.println("Contando até " + contagem +" :");
         for(int cont = 0; cont < contagem; cont ++) {
             System.out.println(cont + 1);
+        }
+    }
+
+    static boolean isValid(int opcao){
+        if(opcao == 2 || opcao == 1){
+            return true;
+        }
+        else{
+            return false;
         }
     }
 }
